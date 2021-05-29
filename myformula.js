ceedb3b1fb56b078a146a8b8f9d8a59f66241c6c
sourabhkumar45 <sourabhkumar766709@gmail.com>
2021-05-29 23:24:36 +0530

    handled case 2 in myformula.js

--- myformula.js
+++ myformula.js
@@ -1,7 +1,10 @@
 "use strict";
 // case 1
 // user enters the formula and based on the expression and value of cells the value is generated
-//and set in the cell.
+// and set in the cell.
+// case 2
+// User changes the value of a parent cell so the change should be reflected in child cell
+// E.g - C1 = A1+B1 change in value of B1 should be reflected back in C1.
 
 function generateAddress(cid, rid) {
   if (rid == undefined && cid == undefined) {
@@ -20,7 +23,14 @@ grid.addEventListener("click", (e) => {
   e.target.addEventListener("blur", () => {
     let { i, j } = generateAddress();
     let cellObj = sheetDB[i][j];
+    // user just click the cell but did not change the value
+    if (cellObj.value == e.target.innerText) {
+      return;
+    }
+    console.log("cell = ", cellObj.value, "target = ", e.target.innerText);
     cellObj.value = e.target.innerText;
+
+    updateMyChildren(cellObj);
   });
 });
 
@@ -28,10 +38,8 @@ formulaCont.addEventListener("keydown", (e) => {
   if (e.key == "Enter" && formulaCont.value) {
     let currentFormula = formulaCont.value;
     let value = evaluateFormula(currentFormula);
-    let cell = findUIElement();
-    cell.value = value;
-    cell.formula = currentFormula;
-    cell.innerText = value;
+    setCellAndDB(value, currentFormula);
+    setChildren(currentFormula, address.value);
   }
 });
 
@@ -48,10 +56,56 @@ function evaluateFormula(currentFormula) {
   return eval(currentFormula);
 }
 // findUIElement returns the cell holding the current address.
-function findUIElement() {
-  let addr = address.value;
-  let cell = document.querySelector(
-    `.cell[cid='${addr[0]}'][rid='${addr[1]}']`
-  );
-  return cell;
+function findUIElement(childAddress) {
+  if (childAddress == undefined) {
+    let addr = address.value;
+    let cell = document.querySelector(
+      `.cell[cid='${addr[0]}'][rid='${addr[1]}']`
+    );
+    return cell;
+  } else {
+    let cell = document.querySelector(
+      `.cell[cid='${childAddress.charAt(0)}'][rid='${childAddress.charAt(1)}']`
+    );
+    return cell;
+  }
+}
+
+function setChildren(currentFormula, childAddress) {
+  let tokens = currentFormula.split(" ");
+  for (let k = 0; k < tokens.length; k++) {
+    let code = tokens[k].charCodeAt(0);
+    if (code >= 65 && code <= 90) {
+      let { i, j } = generateAddress(tokens[k].charAt(0), tokens[k].charAt(1));
+      let parentObj = sheetDB[i][j];
+      parentObj.children.add(childAddress);
+    }
+  }
+}
+
+function setCellAndDB(value, currentFormula, childAddress) {
+  let cell;
+  let addr = { i: 0, j: 0 };
+  if (childAddress == undefined) {
+    cell = findUIElement();
+    addr = generateAddress();
+  } else {
+    cell = findUIElement(childAddress);
+    addr = generateAddress(childAddress.charAt(0), childAddress.charAt(1));
+  }
+  cell.innerText = value;
+  sheetDB[addr.i][addr.j].value = value;
+  sheetDB[addr.i][addr.j].formula = currentFormula;
+}
+
+function updateMyChildren(cellObj) {
+  let { children } = cellObj;
+  for (let child of children) {
+    let { i, j } = generateAddress(child.charAt(0), child.charAt(1));
+    let childObj = sheetDB[i][j];
+    let childFormula = childObj.formula;
+    let newValue = evaluateFormula(childFormula);
+    setCellAndDB(newValue, childFormula, child);
+    updateMyChildren(childObj);
+  }
 }
